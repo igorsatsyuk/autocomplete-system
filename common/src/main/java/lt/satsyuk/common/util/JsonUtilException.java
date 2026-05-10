97e50d19c2d83d4a4a40787e632c49f2438ed979
igorsatsyuk <igor.satsyuk2@gmail.com>
2026-05-10 17:26:03 +0300

    refactor: remove unused JsonUtil

--- common/src/main/java/lt/satsyuk/common/util/JsonUtilException.java
+++
@@ -1,8 +0,0 @@
-package lt.satsyuk.common.util;
-
-public class JsonUtilException extends RuntimeException {
-
-    public JsonUtilException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
